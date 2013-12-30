8ea24a2f8c9e302474e44c8c44588beb0b0c77f5
godgunman <godgunman@gmail.com>
2013-12-30 19:57:22 +0800

    add spinner data

--- 20131230/SimpleRelativeLayout/src/com/example/simplerelativelayout/MainActivity.java
+++ 20131230/SimpleRelativeLayout/src/com/example/simplerelativelayout/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.simplerelativelayout;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+
+	private Spinner month, day;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		month = (Spinner) findViewById(R.id.month);
+		day = (Spinner) findViewById(R.id.day);
+
+		String[] data = getResources().getStringArray(R.array.month);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, data);
+		month.setAdapter(adapter);
 	}
 
 	@Override
