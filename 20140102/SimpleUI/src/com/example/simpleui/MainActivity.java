2cbc170968df7506b9fe0466420476c3598e59e0
godgunman <godgunman@gmail.com>
2014-01-02 20:08:25 +0800

    simpleui project init

---
+++ 20140102/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.simpleui;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.Editable;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private Button submitButton;
+	private EditText inputEdit;
+	private CheckBox isEncrypt;
+	private SharedPreferences sp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
+
+		submitButton = (Button) findViewById(R.id.submitButton);
+		inputEdit = (EditText) findViewById(R.id.input);
+		isEncrypt = (CheckBox) findViewById(R.id.encrypt);
+
+		submitButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Log.d("debug", "click");
+				sendMessage();
+			}
+		});
+
+		inputEdit.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() == KeyEvent.ACTION_DOWN) {
+					if (keyCode == KeyEvent.KEYCODE_ENTER) {
+						Log.d("debug", "enter");
+						sendMessage();
+						return true;
+					} else {
+						SharedPreferences.Editor editor = sp.edit();
+						editor.putString("text", inputEdit.getText().toString());
+						editor.commit();
+					}
+				}
+				return false;
+			}
+		});
+
+		inputEdit.setText(sp.getString("text", ""));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private void sendMessage() {
+		Editable editable = inputEdit.getText();
+		String text = editable.toString();
+		if (isEncrypt.isChecked()) {
+			text = "************";
+		}
+		editable.clear();
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+}
