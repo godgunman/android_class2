69adf3c69e5bc0fc0cdfe68870549863cf0ad73f
godgunman <godgunman@gmail.com>
2014-01-02 21:32:50 +0800

    pass value

--- 20140102/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140102/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends Activity {
 
 		Intent intent = new Intent();
 		intent.setClass(this, MessageActivity.class);
+		intent.putExtra("text", text);
 		startActivity(intent);
 	}
 }
