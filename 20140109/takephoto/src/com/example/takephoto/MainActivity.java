6a560153e4c05f0eeaaa41767d4d1baed2d176f1
godgunman <godgunman@gmail.com>
2014-01-09 20:29:44 +0800

    invoke camera

--- 20140109/takephoto/src/com/example/takephoto/MainActivity.java
+++ 20140109/takephoto/src/com/example/takephoto/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.takephoto;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			return true;
+		case R.id.action_take_photo:
+			Intent intent = new Intent();
+			intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
