a88d3efd14c0565c12fae78c9b7ccccd57a1354b
godgunman <godgunman@gmail.com>
2014-01-09 19:26:54 +0800

    add parsequery

--- 20140106/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140106/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -4,12 +4,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MessageActivity extends Activity {
@@ -22,44 +28,39 @@ public class MessageActivity extends Activity {
 		setContentView(R.layout.activity_message);
 
 		textView = (TextView) findViewById(R.id.textView1);
-
 		String text = getIntent().getStringExtra("text");
-		writeFile(text);
-		
-		textView.setText(readFile());
-		
+
 		ParseObject obj = new ParseObject("message3");
 		obj.put("text", text);
-		obj.saveInBackground();
-	}
-	
-	private String readFile() {
-		try {
-			FileInputStream fis = openFileInput("message");
-			byte[] buffer = new byte[1024];
-			fis.read(buffer);
-			
-			return new String(buffer);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
-	
-	private void writeFile(String text) {
-		try {
-			FileOutputStream fos = openFileOutput("message",
-					Context.MODE_APPEND);
-			text += "\n";
-			fos.write(text.getBytes());
-			fos.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		obj.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e == null) {
+					Log.d("debug", "done");
+				} else {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("message3");
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (e == null) {
+					String all = "";
+					for (ParseObject obj : objects) {
+						all += obj.getString("text");
+					}
+					textView.setText(all);
+				} else {
+					e.printStackTrace();
+				}
+
+			}
+		});
+
 	}
 
 }
