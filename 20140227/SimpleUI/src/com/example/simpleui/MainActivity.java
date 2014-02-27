0001a8335f63f1ded404d6154300f0c488dc4146
godgunman <godgunman@gmail.com>
2014-02-27 21:11:57 +0800

    update

--- 20140227/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140227/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -3,48 +3,67 @@ package com.example.simpleui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	private EditText editText; 
+	private EditText editText;
 	private Button button;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        editText = (EditText) findViewById(R.id.editText1);
-        editText.setHint("type something ...");
-        
-        button = (Button) findViewById(R.id.button1);
-        button.setText("send");
-        button.setOnClickListener(new OnClickListener() {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		editText = (EditText) findViewById(R.id.editText1);
+		editText.setHint("type something ...");
+		editText.setOnKeyListener(new OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				Log.d("debug",
+						"keyOcde =" + keyCode + "keyEvent=" + event.getAction());
+				if (event.getAction() == KeyEvent.ACTION_DOWN) {
+					if (keyCode == KeyEvent.KEYCODE_ENTER) {
+						sendMessage();				
+						return true;
+					}
+				}
+				return false;
+			}
+		});
+
+		button = (Button) findViewById(R.id.button1);
+		button.setText("send");
+		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String text = editText.getText().toString();
-				Log.d("debug", "[button1]" + text);
-				editText.getText().clear();
+				sendMessage();
 			}
-		});//alt + /
-    }
+		});// alt + /
+	}
 
-    public void submit2(View view) {
+	public void submit2(View view) {
+		sendMessage();
+	}
+
+	private void sendMessage() {
 		String text = editText.getText().toString();
-		Log.d("debug", "[button2]" + text);
-		editText.getText().clear();    	    
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		editText.getText().clear();
+		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
