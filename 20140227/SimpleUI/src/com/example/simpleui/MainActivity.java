68ed78b47a09bac94073fdb3db8b5b09f59c79a4
godgunman <godgunman@gmail.com>
2014-02-27 20:39:35 +0800

    implement click

--- 20140227/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140227/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.simpleui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,8 +24,22 @@ public class MainActivity extends Activity {
         
         button = (Button) findViewById(R.id.button1);
         button.setText("send");
+        button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String text = editText.getText().toString();
+				Log.d("debug", "[button1]" + text);
+				editText.getText().clear();
+			}
+		});//alt + /
     }
 
+    public void submit2(View view) {
+		String text = editText.getText().toString();
+		Log.d("debug", "[button2]" + text);
+		editText.getText().clear();    	    
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
