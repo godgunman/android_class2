140f969ab9bdcd80785c762a7dd7364b2dc3c668
godgunman <godgunman@gmail.com>
2014-02-27 21:43:46 +0800

    add simpleUIcontrol

---
+++ 20140227/SimpleUIcontrol/src/com/example/simpleuicontrol/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.simpleuicontrol;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private TextView textView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		textView = (TextView) findViewById(R.id.text);
+	}
+
+	public void moveLeft(View view) {
+		RelativeLayout.LayoutParams params = (LayoutParams) textView
+				.getLayoutParams();
+		params.leftMargin -= 10;
+		textView.setLayoutParams(params);
+	}
+
+	public void moveRight(View view) {
+		RelativeLayout.LayoutParams params = (LayoutParams) textView
+				.getLayoutParams();
+		params.leftMargin += 10;
+		textView.setLayoutParams(params);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
