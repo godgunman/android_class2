b22f475cd1ba5742c8e36c050e102466c49ed174
godgunman <godgunman@gmail.com>
2014-03-27 20:07:50 +0800

    use asynctask

--- 20140327/network/src/com/example/network/MainActivity.java
+++ 20140327/network/src/com/example/network/MainActivity.java
@@ -7,49 +7,76 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
 	private TextView textView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		textView = (TextView) findViewById(R.id.textView1);
+
+		// StrictMode.ThreadPolicy policy = new
+		// StrictMode.ThreadPolicy.Builder()
+		// .permitAll().build();
+		// StrictMode.setThreadPolicy(policy);
 	}
 
 	public void fetch(View view) {
-		String urlStr = "http://yahoo.com";
+		final String urlStr = "http://tw.yahoo.com/";
 
-		try {
-			URL url = new URL(urlStr);
-			URLConnection connection = url.openConnection();
+		AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
 
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(
-					connection.getInputStream()));
-			
-			String result = "";
-			String line;
-			while( (line = buffer.readLine()) != null) {
-				result += line;
+			@Override
+			protected void onPreExecute() {
 			}
 
-			textView.setText(result);
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			@Override
+			protected String doInBackground(Void... params) {
+				try {
+					URL url = new URL(urlStr);
+					URLConnection connection = url.openConnection();
+
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(connection.getInputStream()));
+
+					StringBuilder stringBuilder = new StringBuilder();
+					String line;
+					while ((line = buffer.readLine()) != null) {
+						stringBuilder.append(line);
+					}
+
+					return stringBuilder.toString();
+
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				textView.setText(result);
+			}
+		};
+
+		task.execute();
 	}
 
 	@Override
