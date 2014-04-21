bba0d1576189f497150c1072591b166ccff32383
godgunman <godgunman@gmail.com>
2014-04-21 19:22:22 +0800

    add SimpleRelativeLayout (form 04/17)

---
+++ 20140421/SimpleRelativeLayout/src/com/example/simplerelativelayout/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.simplerelativelayout;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		private Spinner month;
+		private Spinner date;
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+
+			month = (Spinner) rootView.findViewById(R.id.month);
+			date = (Spinner) rootView.findViewById(R.id.date);
+
+			String[] list = getResources().getStringArray(R.array.month);
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+					getActivity(), android.R.layout.simple_spinner_item, list);
+			month.setAdapter(adapter);
+			month.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+				@Override
+				public void onItemSelected(AdapterView<?> adpaterView, View view,
+						int position, long id) {
+					if (position==0) {
+						setDateSpinner(31);
+					} else if(position==1) {
+						setDateSpinner(28);
+					} else if(position==2) {
+						setDateSpinner(31);
+					} else if(position==3) {
+						setDateSpinner(30);
+					} else if(position==4) {
+						setDateSpinner(31);
+					} else if(position==5) {
+						setDateSpinner(30);
+					}
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> arg0) {
+					// TODO Auto-generated method stub
+				}
+			});
+
+			setDateSpinner(31);
+
+			return rootView;
+		}
+
+		private void setDateSpinner(int limit) {
+			String[] list2 = new String[limit];
+			for (int i = 0; i < limit; i++) {
+				list2[i] = String.valueOf(i + 1);
+			}
+
+			ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+					getActivity(), android.R.layout.simple_spinner_item, list2);
+			date.setAdapter(adapter2);
+		}
+	}
+
+}
