87b04753ea041e16404adbeff6a184cd5424e84c
godgunman <godgunman@gmail.com>
2014-04-24 20:28:41 +0800

    add write file method

--- 20140424/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140424/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,11 +1,17 @@
 package com.example.simpleui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MessageActivity extends Activity {
 
+	private static final String FILE_NAME = "text.txt";
 	private TextView textView;
 
 	@Override
@@ -17,5 +23,26 @@ public class MessageActivity extends Activity {
 
 		String text = getIntent().getStringExtra("text");
 		textView.setText(text);
+
+		writeFile(text);
 	}
+
+	private void writeFile(String text) {
+		try {
+			text += "\n";
+
+			FileOutputStream fos = openFileOutput(FILE_NAME,
+					Context.MODE_APPEND);
+			fos.write(text.getBytes());
+			fos.flush();
+			fos.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
