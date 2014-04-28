0289e8be436274a0ecaad1f38671db2d47613e51
godgunman <godgunman@gmail.com>
2014-04-28 19:59:54 +0800

    add SaveCallback

--- 20140428/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140428/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -90,6 +90,7 @@ public class MainActivity extends ActionBarActivity {
 			Intent intent = new Intent();
 			intent.setClass(getActivity(), MessageActivity.class);
 			intent.putExtra("text", text);
+			intent.putExtra("checkBox", checkBox.isChecked());
 			getActivity().startActivity(intent);
 		}
 
--- 20140428/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140428/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,10 +1,14 @@
 package com.example.simpleui;
 
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MessageActivity extends Activity {
 
@@ -18,12 +22,27 @@ public class MessageActivity extends Activity {
 		textView = (TextView) findViewById(R.id.textView1);
 
 		String text = getIntent().getStringExtra("text");
+		boolean isChecked = getIntent().getBooleanExtra("checkBox", false); 
 
 		ParseObject testObject = new ParseObject("Message");
 		testObject.put("text", text);
-		testObject.saveInBackground();
-
+		testObject.put("checkBox", isChecked);
+		testObject.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e == null) {
+					Log.d("debug", "OK");
+				} else {
+					e.printStackTrace();
+				}
+				ok();
+			}
+		});
+		
 		textView.setText(text);
 	}
 
+	private void ok() {
+		Toast.makeText(this, "done.", Toast.LENGTH_SHORT).show();
+	}
 }
