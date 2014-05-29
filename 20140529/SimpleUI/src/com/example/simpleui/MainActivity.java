b99a1113d941d818d3a755b848267d1def5e135c
godgunman <godgunman@gmail.com>
2014-05-29 22:03:56 +0800

    add onkeylistener

--- 20140529/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140529/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -81,6 +83,20 @@ public class MainActivity extends ActionBarActivity {
 				@Override
 				public void onClick(View v) {
 					showToast(editText.getText().toString());
+				}
+			});
+
+			editText.setOnKeyListener(new OnKeyListener() {
+
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+					if (event.getAction() == KeyEvent.ACTION_DOWN) {
+						if (keyCode == KeyEvent.KEYCODE_ENTER) {
+							showToast(editText.getText().toString());
+							return true;
+						}
+					}
+					return false;
 				}
 			});
 
