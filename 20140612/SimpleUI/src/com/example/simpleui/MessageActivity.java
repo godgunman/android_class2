4081e14ca3148417df0ec2fdc21c37f7370bcc95
godgunman <godgunman@gmail.com>
2014-06-12 21:00:51 +0800

    add contentview for MessageActivity

--- 20140612/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140612/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -3,17 +3,23 @@ package com.example.simpleui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
-public class MessageActivity extends Activity{
+public class MessageActivity extends Activity {
 
+	private TextView textView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.activity_message);
+
 		String text = getIntent().getStringExtra("text");
 		boolean isChecked = getIntent().getBooleanExtra("checkbox", false);
+
+		textView = (TextView) findViewById(R.id.textView1);
+		textView.setText(text);
 		
-		Log.d("debug", text + "," + isChecked);
 	}
 }
