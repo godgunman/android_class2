8d977f3a3304b53739c3a3716a6171242784d411
godgunman <godgunman@gmail.com>
2014-06-12 21:41:05 +0800

    add writefile method

--- 20140612/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140612/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,6 +1,11 @@
 package com.example.simpleui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -8,7 +13,7 @@ import android.widget.TextView;
 public class MessageActivity extends Activity {
 
 	private TextView textView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -20,6 +25,23 @@ public class MessageActivity extends Activity {
 
 		textView = (TextView) findViewById(R.id.textView1);
 		textView.setText(text);
-		
+
+		writeFile(text);
+	}
+
+	private void writeFile(String text) {
+		try {
+			FileOutputStream fos = openFileOutput("history.txt",
+					Context.MODE_APPEND);
+			text += "\n";
+			fos.write(text.getBytes());
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
