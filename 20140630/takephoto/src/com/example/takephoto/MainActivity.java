8219e31bed4444623dab7e7b2224a7be019e7272
godgunman <godgunman@gmail.com>
2014-06-30 20:07:30 +0800

    add imageview to show result (dirty code)

--- 20140630/takephoto/src/com/example/takephoto/MainActivity.java
+++ 20140630/takephoto/src/com/example/takephoto/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.os.Build;
 import android.provider.MediaStore;
@@ -18,6 +21,8 @@ import android.provider.MediaStore;
 public class MainActivity extends ActionBarActivity {
 
 	private static final int REQUEST_CODE_TAKE_PHOTO = 1;
+
+	public static ImageView imageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +72,11 @@ public class MainActivity extends ActionBarActivity {
 			Log.d("debug", "onActivityResult, requestCode=" + requestCode
 					+ ", resultCode=" + resultCode);
 			Toast.makeText(this, "from camera", Toast.LENGTH_SHORT).show();
+
+			if (resultCode == RESULT_OK) {
+				Bitmap bitmap = intent.getParcelableExtra("data");
+				imageView.setImageBitmap(bitmap);
+			}
 		}
 	}
 
@@ -83,6 +93,7 @@ public class MainActivity extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
+			imageView = (ImageView) rootView.findViewById(R.id.imageView1);
 			return rootView;
 		}
 	}
