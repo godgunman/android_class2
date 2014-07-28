1205c66064ee403cbfe210cdead1aea3b6b735ac
godgunman <godgunman@gmail.com>
2014-07-28 20:01:33 +0800

    coding layout

--- 20140728/SimpleLinearLayout/src/com/example/simplelinearlayout/MainActivity.java
+++ 20140728/SimpleLinearLayout/src/com/example/simplelinearlayout/MainActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private LinearLayout linearLayout;
+	private Button button;
+	private EditText editText1, editText2, editText3;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        
+        linearLayout = new LinearLayout(this);
+        button = new Button(this);
+        editText1 = new EditText(this);
+        editText2 = new EditText(this);
+        editText3 = new EditText(this);
+    
+        linearLayout.addView(editText1);
+        linearLayout.addView(editText2);
+        linearLayout.addView(editText3);
+        linearLayout.addView(button);
+
+        setContentView(linearLayout);
     }
 
 
