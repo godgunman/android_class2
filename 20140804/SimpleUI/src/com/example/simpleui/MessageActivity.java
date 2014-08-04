b0085065d36a029c1e658c9435086b1eeb29a2ae
godgunman <godgunman@gmail.com>
2014-08-04 21:31:19 +0800

    read file

--- 20140804/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140804/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,5 +1,6 @@
 package com.example.simpleui;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,22 +13,24 @@ import android.widget.TextView;
 public class MessageActivity extends ActionBarActivity {
 
 	private TextView textView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_message);
-		
+
 		textView = (TextView) findViewById(R.id.textView1);
-		
+
 		String text = getIntent().getStringExtra("text");
-		textView.setText(text);
 		writeFile(text);
+		textView.setText(readFile());
 	}
-	
+
 	private void writeFile(String text) {
 		try {
-			FileOutputStream fos = openFileOutput("history.txt", Context.MODE_APPEND);
+			text += "\n";
+			FileOutputStream fos = openFileOutput("history.txt",
+					Context.MODE_APPEND);
 			fos.write(text.getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
@@ -35,6 +38,25 @@ public class MessageActivity extends ActionBarActivity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	private String readFile() {
+		FileInputStream fis;
+		try {
+			fis = openFileInput("history.txt");
+			byte[] buffer = new byte[1024];
+			fis.read(buffer);
+			fis.close();
+
+			return new String(buffer);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
