a1391c5ca997037023c75a035d00e2ef1c3991de
godgunman <godgunman@gmail.com>
2014-08-04 21:01:52 +0800

    wrtie file

--- 20140804/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140804/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,5 +1,10 @@
 package com.example.simpleui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
@@ -17,5 +22,19 @@ public class MessageActivity extends ActionBarActivity {
 		
 		String text = getIntent().getStringExtra("text");
 		textView.setText(text);
+		writeFile(text);
+	}
+	
+	private void writeFile(String text) {
+		try {
+			FileOutputStream fos = openFileOutput("history.txt", Context.MODE_APPEND);
+			fos.write(text.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
