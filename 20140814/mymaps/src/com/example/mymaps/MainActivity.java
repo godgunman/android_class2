429bfa59b8af6941e81c63c765958532f011ed74
godgunman <godgunman@gmail.com>
2014-08-14 21:09:33 +0800

    add marker, move camera

--- 20140814/mymaps/src/com/example/mymaps/MainActivity.java
+++ 20140814/mymaps/src/com/example/mymaps/MainActivity.java
@@ -1,46 +1,53 @@
 package com.example.mymaps;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends ActionBarActivity {
 
-    private SupportMapFragment mapFramgent;
+	private SupportMapFragment mapFramgent;
 	private GoogleMap googleMap;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
 		mapFramgent = (SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.fragment1);
 		googleMap = mapFramgent.getMap();
-    }
 
+		// 25.019051, 121.543695
+		LatLng ntuCsie = new LatLng(25.019051, 121.543695);
+		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ntuCsie, 15));
+		googleMap.addMarker(new MarkerOptions().position(ntuCsie).title(
+				"NTU CSIE"));
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
