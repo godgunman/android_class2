2296a2ee50cf027b796022030e0e1442d2e5ff7b
godgunman <godgunman@gmail.com>
2014-08-18 19:59:09 +0800

    show image and save to parse

--- 20140818/takephoto/src/com/example/takephoto/MainActivity.java
+++ 20140818/takephoto/src/com/example/takephoto/MainActivity.java
@@ -1,22 +1,37 @@
 package com.example.takephoto;
 
-import android.support.v7.app.ActionBarActivity;
+import java.io.ByteArrayOutputStream;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseFile;
+import com.parse.SaveCallback;
 
 public class MainActivity extends ActionBarActivity {
 
 	private static final int REQUEST_CODE_TAKE_PHOTO = 100;
+	private ImageView imageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Parse.initialize(this, "6GIweBfY6S45aUHHhzAkw4cgo6Cb7PlvUyYYwJFs",
+				"nEFIK6PmEiidO3qnyvPa04WCi9rJCECOvN8qg5vf");
+
 		setContentView(R.layout.activity_main);
+
+		imageView = (ImageView) findViewById(R.id.imageView1);
 	}
 
 	@Override
@@ -50,10 +65,28 @@ public class MainActivity extends ActionBarActivity {
 			Log.d("debug", "camera back");
 			if (resultCode == RESULT_OK) {
 				Bitmap bitmap = data.getParcelableExtra("data");
-
+				imageView.setImageBitmap(bitmap);
+				saveToParse(bitmap);
 			}
 		}
 
 		super.onActivityResult(requestCode, requestCode, data);
+	}
+
+	private void saveToParse(Bitmap bitmap) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		bitmap.compress(Bitmap.CompressFormat.PNG, 90, baos);
+		byte[] bytes = baos.toByteArray();
+
+		final ParseFile file = new ParseFile("photo.png", bytes);
+		file.saveInBackground(new SaveCallback() {
+
+			@Override
+			public void done(ParseException e) {
+				String url = file.getUrl();
+				Log.d("debug", url);
+			}
+		});
+
 	}
 }
