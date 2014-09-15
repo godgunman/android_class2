70c82a35eeba16679ed35456f0796f61d08c5f71
godgunman <godgunman@gmail.com>
2014-09-15 21:01:38 +0800

    write file

--- 20140915/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140915/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,8 +1,13 @@
 package com.example.simpleui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class MessageActivity extends Activity {
@@ -20,5 +25,24 @@ public class MessageActivity extends Activity {
 
 		String text = getIntent().getStringExtra("text");
 		textView.setText(text);
+
+		writeFile(text);
+
+	}
+
+	// file location: data/data/com.example.simpleui/files/histroy.txt
+	private void writeFile(String text) {
+
+		try {
+			FileOutputStream fos = openFileOutput("history.txt",
+					Context.MODE_APPEND);
+			fos.write(text.getBytes());
+			fos.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
