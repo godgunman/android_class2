1ec0631407dd4e075111be5155ab76cad18173aa
godgunman <godgunman@gmail.com>
2014-09-22 20:38:56 +0800

    remove read/write file

--- 20140922/SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ 20140922/SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,9 +1,5 @@
 package com.example.simpleui;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,10 +11,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseException;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -38,7 +31,6 @@ public class MessageActivity extends Activity {
 		String text = getIntent().getStringExtra("text");
 		boolean checked = getIntent().getBooleanExtra("checked", false);
 
-		writeFile(text, checked);
 		saveToParse(text, checked);
 		loadMessageFromParse();
 	}
@@ -81,39 +73,5 @@ public class MessageActivity extends Activity {
 				android.R.layout.simple_list_item_2, from, to);
 
 		listView.setAdapter(adapter);
-	}
-
-	// file location: data/data/com.example.simpleui/files/histroy.txt
-	private void writeFile(String text, boolean checked) {
-
-		text += "," + checked + "\n";
-
-		try {
-			FileOutputStream fos = openFileOutput("history.txt",
-					Context.MODE_APPEND);
-			fos.write(text.getBytes());
-			fos.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private String readFile() {
-		try {
-			FileInputStream fis = openFileInput("history.txt");
-			byte[] buffer = new byte[1024];
-			fis.read(buffer);
-			fis.close();
-			return new String(buffer);
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
