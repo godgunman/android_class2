27f3bfe2deef44c06b624671863ac8b58cd242d3
godgunman <godgunman@gmail.com>
2014-09-25 19:58:30 +0800

    implement loadFromParse

--- 20140925/takephoto/src/com/example/takephoto/MainActivity.java
+++ 20140925/takephoto/src/com/example/takephoto/MainActivity.java
@@ -1,27 +1,35 @@
 package com.example.takephoto;
 
 import java.io.ByteArrayOutputStream;
+import java.util.List;
 
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
 
 	private static final int TAKE_PHOTO_REQUEST_CODE = 0;
 	private ImageView imageView;
+	private LinearLayout linearLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,9 @@ public class MainActivity extends ActionBarActivity {
 		Parse.initialize(this, "6GIweBfY6S45aUHHhzAkw4cgo6Cb7PlvUyYYwJFs",
 				"nEFIK6PmEiidO3qnyvPa04WCi9rJCECOvN8qg5vf");
 		imageView = (ImageView) findViewById(R.id.imageView1);
+		linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
+		
+		loadFromParse();
 	}
 
 	@Override
@@ -73,13 +84,43 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
+	private void loadFromParse() {
+
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Photo");
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				for (ParseObject object : objects) {
+					ParseFile file = object.getParseFile("file");
+					try {
+						byte[] data = file.getData();
+						Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0,
+								data.length);
+						ImageView imageView = new ImageView(MainActivity.this);
+						imageView.setImageBitmap(bitmap);
+						linearLayout.addView(imageView);
+
+					} catch (ParseException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+
+	}
+
 	private void saveToParse(Bitmap bitmap) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		bitmap.compress(CompressFormat.PNG, 90, baos);
 		byte[] bytes = baos.toByteArray();
 
 		final ParseFile file = new ParseFile("photo.png", bytes);
-		file.saveInBackground(new SaveCallback() {
+		ParseObject object = new ParseObject("Photo");
+
+		object.put("file", file);
+		object.saveInBackground(new SaveCallback() {
 
 			@Override
 			public void done(ParseException e) {
