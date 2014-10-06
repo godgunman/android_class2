97d9c1214491b8b9cb6add5938648af2425d1c64
godgunman <godgunman@gmail.com>
2014-10-06 20:41:42 +0800

    add project SimpleActFragment

---
+++ 20141006/SimpleActFragment/src/com/example/simpleactfragment/Activity1.java
@@ -0,0 +1,13 @@
+package com.example.simpleactfragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ 20141006/SimpleActFragment/src/com/example/simpleactfragment/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.simpleactfragment;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		private Button button1, button2, button3;
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			button1 = (Button) rootView.findViewById(R.id.button1);
+			button2 = (Button) rootView.findViewById(R.id.button2);
+			button3 = (Button) rootView.findViewById(R.id.button3);
+
+			return rootView;
+		}
+	}
+}
